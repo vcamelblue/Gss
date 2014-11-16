a19d5984116b14dd3a276b8323198a3697e67fe1
pasqualepresutti <paskuale_992@hotmail.it>
2014-11-16 20:27:27 +0100

    Aggiunta Classe Cliente Contatto

---
+++ Gss/Model/Cliente.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gss.Model
+{
+    class Cliente
+    {
+        private string _nome;
+        private string _cognome;
+        private string _codiceFiscale;
+        private DateTime _dataNascita;
+        private List<Contatto> _contatti;
+
+        public Cliente(string nome, string cognome, string codiceFiscale, DateTime dataNascita, Contatto contatto)
+        {
+            _nome = nome;
+            _cognome = cognome;
+            _codiceFiscale = codiceFiscale;
+            _dataNascita = dataNascita;
+            _contatti = new List<Contatto>();
+            _contatti.Add(contatto);
+        }
+
+        public string Nome
+        {
+            get { return _nome; }
+            set { _nome = value; }
+        }
+
+        public string Cognome
+        {
+            get { return _cognome; }
+            set { _cognome = value; }
+        }
+
+        public string CodiceFiscale
+        {
+            get { return _codiceFiscale; }
+            set { _codiceFiscale = value; }
+        }
+
+        public DateTime DataNascita
+        {
+            get { return _dataNascita; }
+            set { _dataNascita = value; }
+        }
+
+        public List<Contatto> Contatti
+        {
+            get { return _contatti; }
+            set { _contatti = value; }
+        }
+
+        public void add(Contatto contatto)
+        {
+            Contatti.Add(contatto);
+        }
+
+        public bool remove(Contatto contatto)
+        {
+            if (Contatti.Count == 1) return false;
+            else return Contatti.Remove(contatto);
+        }
+
+
+    }
+}
---
+++ Gss/Model/Contatto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gss.Model
+{
+    class Contatto
+    {
+        private string _tipo;
+        private string _valore;
+
+        public string Tipo
+        {
+            get { return _tipo; }
+            set { _tipo = value; }
+        }
+
+        public string Valore
+        {
+            get { return _valore; }
+            set { _valore = value; }
+        }
+
+    }
+}

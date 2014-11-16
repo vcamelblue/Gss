f02affaf82fd03729dacae90f39bf1c1f267b054
pasqualepresutti <paskuale_992@hotmail.it>
2014-11-16 20:05:33 +0100

    Aggiunta Classe Impianti

---
+++ Gss/Model/Impianti.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gss.Model
+{
+    class Impianti
+    {
+        private List<Impianto> _impianti;
+
+        public Impianti()
+        {
+            _impianti = new List<Impianto>();
+        }
+
+        public void Add(Impianto impianto)
+        {
+            ListaImpianti.Add(impianto);
+        }
+
+        public bool Remove(Impianto impianto)
+        {
+            return ListaImpianti.Remove(impianto);
+        }
+
+        public List<Impianto> ListaImpianti
+        {
+            get { return _impianti; }
+            set { _impianti = value; }
+        }
+
+
+    }
+}

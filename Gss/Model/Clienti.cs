f1c9a3191616889b9498277f39e8ebab87b4913f
pasqualepresutti <paskuale_992@hotmail.it>
2014-11-16 20:48:06 +0100

    Aggiunta Classe Clienti
    Senza remove

---
+++ Gss/Model/Clienti.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gss.Model
+{
+    class Clienti
+    {
+
+        private List<Cliente> _clienti;
+
+        public Clienti(List<Cliente> clienti)
+        {
+            _clienti = new List<Cliente>();
+            _clienti = clienti;
+        }
+
+        public void Add(Cliente cliente)
+        {
+            ListaClienti.Add(cliente);
+        }
+
+        public List<Cliente> ListaClienti
+        {
+            get { return _clienti; }
+            set { _clienti = value; }
+        }
+       
+
+    }
+}

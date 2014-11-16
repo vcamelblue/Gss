4d3fd370f7487c914ac693368e8f9d09800ea8ff
pasqualepresutti <paskuale_992@hotmail.it>
2014-11-16 18:14:51 +0100

    Aggiunta Classe Pista

---
+++ Gss/Model/Pista.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gss.Model
+{
+    using System;
+
+    public abstract class Pista
+    {
+        private string _nome;
+
+        public Pista(string nome)
+        {
+            _nome = nome;
+        }
+
+        public string Nome
+        {
+            get
+            {
+                return _nome;
+            }
+
+            set
+            {
+                _nome = value;
+            }
+        }
+
+    }
+}

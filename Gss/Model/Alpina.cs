72580dfe5db8f872adcca24c8d91f77ad04e41d3
pasqualepresutti <paskuale_992@hotmail.it>
2014-11-16 19:31:38 +0100

    Aggiunta Classe Alpina

---
+++ Gss/Model/Alpina.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gss.Model;
+
+namespace Gss.Model
+{
+    class Alpina : Pista
+    {
+        enum Difficolta {Bassa, Media, Alta};
+
+        private Difficolta _difficolta;
+
+        public Alpina(string nome, Difficolta difficolta) : base(nome) 
+        {
+            _difficolta = difficolta;
+        }
+
+        private Difficolta Difficolta
+        {
+            get { return _difficolta; }
+            set { _difficolta = value; }
+        }
+
+
+    }
+}

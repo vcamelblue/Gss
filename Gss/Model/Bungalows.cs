5ee1c4cc388b7f517eb713274323c5a5e433ddea
pasqualepresutti <paskuale_992@hotmail.it>
2014-11-17 11:23:22 +0100

    Aggiunta Classe Bungalows

---
+++ Gss/Model/Bungalows.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gss.Model
+{
+    class Bungalows
+    {
+        private List<Bungalow> _bungalows;
+
+        public Bungalows()
+        {
+            _bungalows = new List<Bungalow>();
+        }
+
+        public List<Bungalow> ListaBungalow
+        {
+            get { return _bungalows; }
+            set { _bungalows = value; }
+        }
+
+        public void add(Bungalow bungalow)
+        {
+            ListaBungalow.Add(bungalow);
+        }
+
+        public bool remove(Bungalow bungalow)
+        {
+            return ListaBungalow.Remove(bungalow);
+        }
+
+        public Bungalows GetBungalowConPostiStandard(int n)
+        {
+            Bungalows result = new Bungalows();
+            foreach (Bungalow b in ListaBungalow)
+            {
+                if (b.postiTotaliStandard() == n)
+                {
+                    result.add(b);
+                }
+            }
+            return result;
+        }
+
+        public Bungalows GetBungalowConPostiMin(int n)
+        {
+            Bungalows result = new Bungalows();
+            foreach (Bungalow b in ListaBungalow)
+            {
+                if (b.postiTotaliMin() == n)
+                {
+                    result.add(b);
+                }
+            }
+            return result;
+        }
+
+        public Bungalows GetBungalowConPostiMax(int n)
+        {
+            Bungalows result = new Bungalows();
+            foreach (Bungalow b in ListaBungalow)
+            {
+                if (b.postiTotaliMax() == n)
+                {
+                    result.add(b);
+                }
+            }
+            return result;
+        }
+
+    }
+}

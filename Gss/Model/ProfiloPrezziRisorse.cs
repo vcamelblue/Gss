97c1aed6d04a2d035cac4e41e16de3667a9f2e0a
totob92 <totob92@hotmail.it>
2014-11-21 16:52:22 +0100

    Aggiunta Classe ProfiloPrezziRisorse

---
+++ Gss/Model/ProfiloPrezziRisorse.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gss.Model
+{
+    class ProfiloPrezziRisorse
+    {
+        string _nome;
+        Dictionary<Risorsa, PrezziRisorsa> _prezziRisorsa;
+
+        public Dictionary<Risorsa, PrezziRisorsa> PrezziRisorsa
+        {
+            get { return _prezziRisorsa; }
+            set { _prezziRisorsa = value; }
+        }
+
+        public string Nome
+        {
+            get { return _nome; }
+            set { _nome = value; }
+        }
+
+        public ProfiloPrezziRisorse(string nome)
+        {
+            _nome = nome;
+            _prezziRisorsa = new Dictionary<Risorsa, PrezziRisorsa>();
+        }
+
+        public void Add(Risorsa risorsa, PrezziRisorsa prezziRisorsa)
+        {
+            _prezziRisorsa.Add(risorsa, prezziRisorsa);
+        }
+
+        public bool Remove(Risorsa risorsa)
+        {
+            return _prezziRisorsa.Remove(risorsa);
+        }
+
+        public PrezziRisorsa getPrezziRisorsa(Risorsa risorsa)
+        {
+            return _prezziRisorsa[risorsa];
+        }
+    }
+}

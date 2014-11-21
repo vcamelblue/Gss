7dbb6c784232ac3949243ffb39690ae8b92507a6
pasqualepresutti <paskuale_992@hotmail.it>
2014-11-21 17:53:43 +0100

    Aggiunta Classe Skicard

--- Gss/Model/SkiCard.cs
+++ Gss/Model/SkiCard.cs
@@ -8,6 +8,53 @@ namespace Gss.Model
 {
     class SkiCard
     {
+        private string _codice;
+        private List<SkiPass> _skiPass;
 
+        public SkiCard(string codice)
+        {
+            _codice = codice;
+            _skiPass = new List<SkiPass>();
+        }
+
+        public string Codice
+        {
+            get { return _codice; }
+            set { _codice = value; }
+        }
+
+        public List<SkiPass> SkiPass
+        {
+            get { return _skiPass; }
+            set { _skiPass = value; }
+        }
+
+        public bool add(SkiPass skipass)
+        {
+            foreach (SkiPass s in SkiPass)
+            {
+                if (s.Equals(skipass))
+                {
+                    return false;
+                }
+            }
+            SkiPass.Add(skipass);
+            return true;
+        }
+
+        public bool remove(SkiPass skipass)
+        {
+            return SkiPass.Remove(skipass);
+        }
+
+        public double prezzoSkicard()
+        {
+            double result = 0;
+            foreach (SkiPass s in SkiPass)
+            {
+                result += s.prezzoSkiPass();
+            }
+            return result;
+        }
     }
 }

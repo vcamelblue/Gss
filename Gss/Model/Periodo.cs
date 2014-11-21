b3cd9ab52ffb0b646a28d104a4ce540bec940955
totob92 <totob92@hotmail.it>
2014-11-21 17:21:57 +0100

    Aggiunta Classe Periodo

---
+++ Gss/Model/Periodo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gss.Model
+{
+    class Periodo
+    {
+        private ProfiloPrezziRisorse _profilo;
+        private DateTime _dataInizio;
+        private DateTime _dataFine;
+
+        public DateTime DataInizio
+        {
+            get { return _dataInizio; }
+            set { _dataInizio = value; }
+        }
+
+        public DateTime DataFine
+        {
+            get { return _dataFine; }
+            set { _dataFine = value; }
+        }
+
+        public bool IsInPeriodo(DateTime data)
+        {
+            if (_dataInizio.CompareTo(data) >= 0 && _dataFine.CompareTo(data) <= 0)
+                return true;
+            return false;
+        }
+        
+    }
+}

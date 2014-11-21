016a472ec95b3e54f87bd7615471329a915a7aa3
vcamelblue <vincvill@hotmail.it>
2014-11-21 19:14:33 +0100

    Cambaita classe Fattura in public

--- Gss/Model/Fattura.cs
+++ Gss/Model/Fattura.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Gss.Model {
+namespace Gss.Model 
+{
 
-    class Fattura {
+    public class Fattura 
+    {
 
         //Fields
 

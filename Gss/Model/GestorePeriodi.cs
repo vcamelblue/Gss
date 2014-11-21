fa0112500c6e7941ed61b6ecb6cf451be2f02309
vcamelblue <vincvill@hotmail.it>
2014-11-21 19:13:41 +0100

    Completata classe Gestione Periodi
    Modificate anche altre cose leggere

--- Gss/Model/GestorePeriodi.cs
+++ Gss/Model/GestorePeriodi.cs
@@ -6,11 +6,21 @@ using System.Threading.Tasks;
 
 namespace Gss.Model
 {
-    class GestorePeriodi
+    public class GestorePeriodi
     {
+
+        //Fields
+
         private static GestorePeriodi _gestorePeriodi;
+        private List<Periodo> periodi = new List<Periodo>();
+
+
+        //Constructors
 
         private GestorePeriodi() { }
+
+
+        //Methods
 
         public GestorePeriodi GetInstance()
         {
@@ -21,7 +31,54 @@ namespace Gss.Model
             return _gestorePeriodi;
         }
 
-        
+
+        public void Add(Periodo p)
+        {
+            this.periodi.Add(p);
+        }
+
+        public bool Remove(Periodo p) 
+        {
+            return this.periodi.Remove(p);
+        }
+
+        public Periodo getPeriodoByData(DateTime data)
+        {
+            if( data == null)
+                return null;
+            
+            foreach(Periodo p in periodi)
+            {
+                if(p.IsDataInPeriodo(data))
+                    return p;
+            }
+            return null;
+
+        }
+
+        public PrezziRisorsa getPrezzoRisorsaByData(Risorsa risorsa, DateTime data)
+        {
+            if (risorsa == null || data == null)
+                return null;
+
+            Periodo periodo = getPeriodoByData(data);
+
+            if (periodo != null) 
+            {
+                return periodo.Profilo.GetPrezziRisorsa(risorsa);
+            }
+
+            return null;
+        }
+
+
+        //Property
+
+        public List<Periodo> Periodi 
+        {
+            get { return periodi; }
+            set { periodi = value; }
+        }
     }
 
     

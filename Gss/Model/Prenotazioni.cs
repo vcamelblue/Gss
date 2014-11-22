fffae682cd8d35e3ceb7aca011b6d7647cbca899
pasqualepresutti <paskuale_992@hotmail.it>
2014-11-22 11:53:31 +0100

    Aggiunta Classe Prenotazioni

---
+++ Gss/Model/Prenotazioni.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gss.Model
+{
+    class Prenotazioni
+    {
+        private List<Prenotazione> _listaPrenotazioni;
+
+        public Prenotazioni(List<Prenotazione> prenotazioni)
+        {
+            _listaPrenotazioni = prenotazioni;
+        }
+
+        public Prenotazioni(){
+            _listaPrenotazioni = new List<Prenotazione>();
+        }
+
+        public List<Prenotazione> ListaPrenotazioni
+        {
+            get { return _listaPrenotazioni; }
+            set { _listaPrenotazioni = value; }
+        }
+
+    }
+}

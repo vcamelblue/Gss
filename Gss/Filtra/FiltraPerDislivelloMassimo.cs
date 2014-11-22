4c7fae5c8b7b281a89ea4a10f02ca2714c06adb7
pasqualepresutti <paskuale_992@hotmail.it>
2014-11-22 13:14:44 +0100

    Preparate Le Signature dei filtri

---
+++ Gss/Filtra/FiltraPerDislivelloMassimo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gss.Model;
+
+namespace Gss.Filtra
+{
+    class FiltraPerDislivelloMassimo :IFiltra
+    {
+        public Impianti Filtra(Impianti impianti)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Gss/Filtra/FiltraPerNome.cs
@@ -0,0 +1,17 @@
+﻿using Gss.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gss.Filtra
+{
+    class FiltraPerNome : IFiltra
+    {
+        public Impianti Filtra(Impianti impianti)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

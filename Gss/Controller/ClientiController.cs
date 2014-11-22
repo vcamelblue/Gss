9d5d06659e7f6dc72b4fd8daa7c1f84577c6ea8e
pasqualepresutti <paskuale_992@hotmail.it>
2014-11-22 18:39:44 +0100

    Aggiuta La classe clienticontroller e la add

---
+++ Gss/Controller/ClientiController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gss.Model;
+
+namespace Gss.Controller
+{
+    class ClientiController : Controller
+    {
+        public ClientiController()
+            : base()
+        {
+
+        }
+
+        public void AddCliente(Cliente cliente)
+        {
+            if (!(Gss.Clienti.Add(cliente)))
+            {
+                throw new Exception("Cliente già registrato");
+            }
+        }
+    }
+}
---
+++ Gss/Controller/PrenotazioniController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gss.Controller
+{
+    class PrenotazioniController : Controller
+    {
+        public PrenotazioniController()
+            :base()
+        {
+
+        }
+
+
+    }
+}

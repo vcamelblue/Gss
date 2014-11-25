4c35959254eeddd19ceffccdf480ad9e15e99f86
vcamelblue <vincvill@hotmail.it>
2014-11-25 11:42:30 +0100

    Aggiunto Componente MyComboBox

---
+++ Gss/View/Components/MyComboBox.Designer.cs
@@ -0,0 +1,40 @@
+﻿namespace Gss.View.Components {
+    partial class MyComboBox {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione componenti
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent() {
+            this.SuspendLayout();
+            // 
+            // MyComboBox
+            // 
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(231)))), ((int)(((byte)(231)))));
+            this.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Size = new System.Drawing.Size(121, 25);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Gss/View/Components/MyComboBox.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gss.View.Components {
+    public partial class MyComboBox : System.Windows.Forms.ComboBox {
+        public MyComboBox() {
+            InitializeComponent();
+        }
+
+        public MyComboBox(IContainer container) {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}

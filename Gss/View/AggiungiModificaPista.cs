9dce205049c6afe9723c1e8a92fff570d6ccec60
vcamelblue <vincvill@hotmail.it>
2014-11-26 19:05:24 +0100

    Corrette le stronzate "alla presutti"
    "vabbè pasquà, alla fin fine voi c'avete villani che è bravo a
    programmà!"
    .....

--- Gss/View/AggiungiModificaPista.cs
+++ Gss/View/AggiungiModificaPista.cs
@@ -17,8 +17,8 @@ namespace Gss.View {
         private Impianto _impianto;
         private Pista _pista;
         private ResortController _resortController;
-        private int _indice;
 
+        //Costruttore per aggiunta
         public AggiungiModificaPista(ResortController resortController, Impianto impianto ) {
 
             _inEditingMode = false;
@@ -30,6 +30,8 @@ namespace Gss.View {
             tabControlWithoutHeader1.SelectedTab = tabVuoto;
         }
 
+        //Costruttore per modifica, si assume che la pista passata sia il riferimento all'oggetto corretto,
+        //da non modificare prima dell'effettivo salvataggio!
         public AggiungiModificaPista(ResortController resortController, Impianto impianto, Pista pista)
         {
             _inEditingMode = true;
@@ -39,17 +41,22 @@ namespace Gss.View {
 
             InitializeComponent();
 
+            //cerco il pannello corretto in base al tipo di pista, se nullo errore!
             TabPage tabCorrente = checkPista(_pista);
-            if (tabCorrente == null)
+
+            if (tabCorrente != null)
             {
-              MessageBox.Show("Pista da modificare non valida");
+                tabControlWithoutHeader1.SelectedTab = checkPista(_pista);
+
+                this.Text = "Modifica Pista";
+                salvaButton.Text = "Salva Modifiche";
+            }
+            else
+            {
+              MessageBox.Show("Pista da modificare non valida!");
+              this.Close();
             }
 
-            tabControlWithoutHeader1.SelectedTab = checkPista(_pista);
-
-
-            this.Text = "Modifica Pista";
-            salvaButton.Text = "Salva Modifiche";
         }
 
         private void salvaButton_Click(object sender, EventArgs e)
@@ -81,31 +88,31 @@ namespace Gss.View {
                 {
                     numeroSalti = int.Parse(numeroSaltiTextBox.Text);
                     numeroJibs = int.Parse(numeroJibsTextBox.Text);
+
                     if (_inEditingMode)
                     {
-                        SnowPark snowparkModificato = new SnowPark(nomePista, numeroSalti, numeroJibs);
                         SnowPark snowpark = (SnowPark)_pista;
-                        snowpark.Nome = snowparkModificato.Nome;
-                        snowpark.NumeroSalti = snowparkModificato.NumeroSalti;
-                        snowpark.NumeroJibs = snowparkModificato.NumeroJibs;
+                        snowpark.Nome = nomePista;
+                        snowpark.NumeroSalti = numeroSalti;
+                        snowpark.NumeroJibs = numeroJibs;
                     }
                     else
                     {
                         SnowPark snowpark = new SnowPark(nomePista, numeroSalti, numeroJibs);
                         _impianto.Add(snowpark);
                     }
-                    //SE TUTTO OK FARE COSI'
 
                     this.DialogResult = DialogResult.OK;
                     this.Close();
+
                 }
-                catch (FormatException exception)
+                catch (FormatException)
                 {
-                    MessageBox.Show("Inserire interi positivi");
+                    MessageBox.Show("Inserire numeri interi positivi!");
                 }
-                catch (ArgumentNullException exception)
+                catch (ArgumentNullException)
                 {
-                    MessageBox.Show("Riempire tutti i campi");
+                    MessageBox.Show("Riempire tutti i campi!");
                 }
                 catch (Exception exception)
                 {
@@ -114,9 +121,8 @@ namespace Gss.View {
             }
             else
             {
-                MessageBox.Show("Inserire un nome valido");
-            }               
-         
+                MessageBox.Show("Inserisci un nome valido per la pista!");
+            }
         }
 
         private void salvaAFondo()
@@ -134,30 +140,28 @@ namespace Gss.View {
                     dislivelloMedio = double.Parse(dislivelloMedioTextBox.Text);
                     if (_inEditingMode)
                     {
-                        Fondo fondoModificata = new Fondo(nomePista, dislivelloMassimo, dislivelloMedio);
                         Fondo fondo = (Fondo)_pista;
-                        fondo.Nome = fondoModificata.Nome;
-                        fondo.DislivelloMassimo = fondoModificata.DislivelloMassimo;
-                        fondo.DislivelloMedio = fondo.DislivelloMedio;
+                        fondo.Nome = nomePista;
+                        fondo.DislivelloMassimo = dislivelloMassimo;
+                        fondo.DislivelloMedio = dislivelloMedio;
                     }
                     else
                     {
                         Fondo fondo = new Fondo(nomePista, dislivelloMassimo, dislivelloMedio);
                         _impianto.Add(fondo);
                     }
-                    //SE TUTTO OK FARE COSI'
 
                     this.DialogResult = DialogResult.OK;
                     this.Close();
 
                 }
-                catch (FormatException exception)
+                catch (FormatException)
                 {
-                    MessageBox.Show("Inserire interi positivi");
+                    MessageBox.Show("Inserire numeri interi positivi!");
                 }
-                catch (ArgumentNullException exception)
+                catch (ArgumentNullException)
                 {
-                    MessageBox.Show("Riempire i campi dei dislivelli");
+                    MessageBox.Show("Riempire i campi dei dislivelli!");
                 }
                 catch (Exception exception)
                 {
@@ -166,7 +170,7 @@ namespace Gss.View {
             }
             else
             {
-                MessageBox.Show("Inserire un nome valido");
+                MessageBox.Show("Inserisci un nome valido per la pista!");
             }               
          
        }
@@ -178,31 +182,30 @@ namespace Gss.View {
             string difficoltaString = difficoltaComboBox.SelectedItem.ToString();
             Difficolta difficoltaValue = 0;
 
-            if (nomePista!="")
+            if (nomePista != "")
             {
                 try
                 {
                     difficoltaValue = (Difficolta)Enum.Parse(typeof(Difficolta), difficoltaString);
+
                     if (_inEditingMode)
                     {
-                        Alpina alpinaModificata = new Alpina(nomePista, difficoltaValue);
                         Alpina alpina = (Alpina)_pista;
-                        alpina.Nome = alpinaModificata.Nome;
-                        alpina.Difficolta = alpinaModificata.Difficolta;
+                        alpina.Nome = nomePista;
+                        alpina.Difficolta = difficoltaValue;
                     }
                     else
                     {
                         Alpina alpina = new Alpina(nomePista, difficoltaValue);
                         _impianto.Add(alpina);
                     }
-                    //SE TUTTO OK FARE COSI'
 
                     this.DialogResult = DialogResult.OK;
                     this.Close();
                 }
-                catch(ArgumentException Exception)
+                catch(ArgumentException)
                 {
-                    MessageBox.Show("Inserire una difficoltà valida");
+                    MessageBox.Show("Inserisci una difficoltà valida!");
                 }
                 catch (Exception exception)
                 {
@@ -211,7 +214,7 @@ namespace Gss.View {
             }
             else
             {
-                MessageBox.Show("Inserire un nome valido");
+                MessageBox.Show("Inserisci un nome valido per la pista!");
             }               
         }
 
@@ -220,11 +223,14 @@ namespace Gss.View {
             this.Close();
         }
 
+
+        //On Load Setto i vaori dei combobox, se alpina setto anche quello dentro il pannello
         private void AggiungiModificaPista_Load(object sender, EventArgs e)
         {
             tipologiaComboBox1.Items.Insert(0, "Alpina");
             tipologiaComboBox1.Items.Insert(1, "Fondo");
             tipologiaComboBox1.Items.Insert(2, "Snowpark");
+
             tipologiaComboBox1.SelectedIndex = 0;
 
             difficoltaComboBox.Items.Insert(0,Difficolta.Alta.ToString());
@@ -235,55 +241,58 @@ namespace Gss.View {
             {
                 tipologiaComboBox1.SelectedIndex = tipologiaComboBox1.FindStringExact(cercaCategoria(_pista));
                 tipologiaComboBox1.Enabled =  false;
+
                 if (_pista is Alpina)
                 {
                     Alpina alpina = (Alpina)_pista;
                     difficoltaComboBox.SelectedIndex = difficoltaComboBox.FindStringExact(alpina.Difficolta.ToString());
                 }
             }
-
         }
 
+
+        //funzione per identificare il pannello in base alla tipologia di pista
         private TabPage checkPista(Pista pista)
         {
             if (pista is Alpina)
             {
                 Alpina alpina = (Alpina)pista;
-                _indice = 0;
+
                 nomeAlpinaTextBox.Text = alpina.Nome;
+
                 return tabAlpina;
             }
-
             if (pista is Fondo)
             {
                 Fondo fondo = (Fondo)pista;
-                _indice = 1;
+
                 nomeFondoTextBox.Text = fondo.Nome;
                 dislivelloMaxTextBox2.Text = fondo.DislivelloMassimo.ToString();
                 dislivelloMedioTextBox.Text = fondo.DislivelloMedio.ToString();
+
                 return tabFondo;
             }
             if (pista is SnowPark)
             {
                 SnowPark snowPark = (SnowPark)pista;
+
                 nomeSnowparkTextBox.Text = snowPark.Nome;
                 numeroSaltiTextBox.Text = snowPark.NumeroSalti.ToString();
                 numeroJibsTextBox.Text = snowPark.NumeroJibs.ToString();
-                _indice = 2;
+
                 return tabSnowpark;
             }
-            else return null;
-
+            return null;
         }
 
+
+        //Funzione per riconoscere Tipo Pista
         private string cercaCategoria(Pista pista)
         {
             if (pista is Alpina)
             {
-               
                 return "Alpina";
             }
-
             if (pista is Fondo)
             {
                 return "Fondo";
@@ -292,10 +301,11 @@ namespace Gss.View {
             {
                 return "SnowPark";
             }
-            else return "";
-
+            return "";
         }
 
+
+        //Funzione per cambiare il pannello
         private void tipologiaComboBox1_SelectedValueChanged(object sender, EventArgs e)
         {
             if (tipologiaComboBox1.SelectedIndex == 0)

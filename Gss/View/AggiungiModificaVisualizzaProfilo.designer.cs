390f69a7027a938ce2e426176885058137d82db9
vcamelblue <vincvill@hotmail.it>
2014-11-26 11:44:01 +0100

    Aggiungta interfaccia Aggiungi Modifica Visualizza Profilo

---
+++ Gss/View/AggiungiModificaVisualizzaProfilo.designer.cs
@@ -0,0 +1,43 @@
+﻿namespace Gss.View {
+    partial class AggiungiModificaVisualizzaProfilo {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.SuspendLayout();
+            // 
+            // AggiungiModificaVisualizzaProfilo
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(251)))), ((int)(((byte)(251)))));
+            this.ClientSize = new System.Drawing.Size(832, 503);
+            this.Font = new System.Drawing.Font("Calibri", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "AggiungiModificaVisualizzaProfilo";
+            this.Text = "Aggiungi Profilo";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}

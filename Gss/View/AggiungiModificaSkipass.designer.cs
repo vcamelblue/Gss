ab9a78b99910ab1bc2d360707a3433f1a96b8c39
vcamelblue <vincvill@hotmail.it>
2014-11-27 15:38:53 +0100

    Aggiunta Interfaccia Aggiungi modifica Skipass

---
+++ Gss/View/AggiungiModificaSkipass.designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Gss.View {
+    partial class AggiungiModificaSkipass {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.myLabel1 = new Gss.View.Components.MyLabel(this.components);
+            this.myLabel2 = new Gss.View.Components.MyLabel(this.components);
+            this.myTextBox1 = new Gss.View.Components.MyTextBox(this.components);
+            this.myComboBox1 = new Gss.View.Components.MyComboBox(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tabControlWithoutHeader1 = new Gss.View.Components.TabControlWithoutHeader(this.components);
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.tabControlWithoutHeader1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // myLabel1
+            // 
+            this.myLabel1.AutoSize = true;
+            this.myLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.myLabel1.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.myLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(86)))), ((int)(((byte)(86)))), ((int)(((byte)(86)))));
+            this.myLabel1.Location = new System.Drawing.Point(32, 31);
+            this.myLabel1.Name = "myLabel1";
+            this.myLabel1.Size = new System.Drawing.Size(77, 21);
+            this.myLabel1.TabIndex = 0;
+            this.myLabel1.Text = "myLabel1";
+            // 
+            // myLabel2
+            // 
+            this.myLabel2.AutoSize = true;
+            this.myLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.myLabel2.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.myLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(86)))), ((int)(((byte)(86)))), ((int)(((byte)(86)))));
+            this.myLabel2.Location = new System.Drawing.Point(401, 31);
+            this.myLabel2.Name = "myLabel2";
+            this.myLabel2.Size = new System.Drawing.Size(77, 21);
+            this.myLabel2.TabIndex = 1;
+            this.myLabel2.Text = "myLabel2";
+            // 
+            // myTextBox1
+            // 
+            this.myTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.myTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.myTextBox1.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.myTextBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(86)))), ((int)(((byte)(86)))), ((int)(((byte)(86)))));
+            this.myTextBox1.Location = new System.Drawing.Point(141, 31);
+            this.myTextBox1.Name = "myTextBox1";
+            this.myTextBox1.Size = new System.Drawing.Size(100, 21);
+            this.myTextBox1.TabIndex = 2;
+            // 
+            // myComboBox1
+            // 
+            this.myComboBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(231)))), ((int)(((byte)(231)))));
+            this.myComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.myComboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.myComboBox1.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.myComboBox1.FormattingEnabled = true;
+            this.myComboBox1.Location = new System.Drawing.Point(517, 31);
+            this.myComboBox1.Name = "myComboBox1";
+            this.myComboBox1.Size = new System.Drawing.Size(121, 26);
+            this.myComboBox1.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.tabControlWithoutHeader1);
+            this.groupBox1.Location = new System.Drawing.Point(0, 63);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(746, 617);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // tabControlWithoutHeader1
+            // 
+            this.tabControlWithoutHeader1.Controls.Add(this.tabPage1);
+            this.tabControlWithoutHeader1.Controls.Add(this.tabPage2);
+            this.tabControlWithoutHeader1.Font = new System.Drawing.Font("Calibri", 9F);
+            this.tabControlWithoutHeader1.Location = new System.Drawing.Point(26, 22);
+            this.tabControlWithoutHeader1.Name = "tabControlWithoutHeader1";
+            this.tabControlWithoutHeader1.SelectedIndex = 0;
+            this.tabControlWithoutHeader1.Size = new System.Drawing.Size(700, 86);
+            this.tabControlWithoutHeader1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 27);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(692, 55);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 27);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(692, 55);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton1);
+            this.groupBox2.Location = new System.Drawing.Point(26, 143);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(683, 252);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radioButton1.Location = new System.Drawing.Point(20, 32);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(102, 21);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // AggiungiModificaSkipass
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(251)))), ((int)(((byte)(251)))));
+            this.ClientSize = new System.Drawing.Size(742, 727);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.myComboBox1);
+            this.Controls.Add(this.myTextBox1);
+            this.Controls.Add(this.myLabel2);
+            this.Controls.Add(this.myLabel1);
+            this.Font = new System.Drawing.Font("Calibri", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "AggiungiModificaSkipass";
+            this.Text = "MyForm";
+            this.groupBox1.ResumeLayout(false);
+            this.tabControlWithoutHeader1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Components.MyLabel myLabel1;
+        private Components.MyLabel myLabel2;
+        private Components.MyTextBox myTextBox1;
+        private Components.MyComboBox myComboBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private Components.TabControlWithoutHeader tabControlWithoutHeader1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}

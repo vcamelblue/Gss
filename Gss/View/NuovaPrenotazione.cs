eb9b46bb528f7b1cb517f69ccee253822d5615c6
pasqualepresutti <paskuale_992@hotmail.it>
2014-11-29 19:00:15 +0100

    Completato Anche il tab prenotazione in Nuova prenotazione

--- Gss/View/NuovaPrenotazione.cs
+++ Gss/View/NuovaPrenotazione.cs
@@ -19,6 +19,7 @@ namespace Gss.View
         private ClientiController clientiController;
         private ResortController resortController;
         private Cliente clienteSelezionato = null;
+        private Prenotazione prenotazioneCorrente = null;
 
         private Button previusSelectedButton = null;
         private Color normalButtonColor = Color.FromKnownColor(KnownColor.LightGray);
@@ -111,6 +112,12 @@ namespace Gss.View
             {
                 tabControlWithoutHeader.SelectedTab = skicardsTabPage;
                 selectRightTab(skicardsTabButton);
+
+                //Recupero i campi per creare la prenotazione
+                int numeroPrenotazione = int.Parse(numeroPrenotazioneTextBox.Text);
+                int numeroPersone = int.Parse(numeroPersoneTextBox.Text);
+                Bungalow bungalowSelezionato = GetBungalowSelezionato();
+                prenotazioneCorrente = new PrenotazioneAttiva(numeroPrenotazione, numeroPersone, dataInizioPrenotazioneTimePicker.Value, dataFinePrenotazioneTimePicker.Value, clienteSelezionato);
             }
             else if (tabControlWithoutHeader.SelectedTab == skicardsTabPage) // sono nella schermata skicards
             {
@@ -123,6 +130,18 @@ namespace Gss.View
 
             }
             
+        }
+
+        private Bungalow GetBungalowSelezionato()
+        {
+            foreach (Bungalow b in resortController.GetBungalows().ListaBungalow)
+            {
+                if (b.Codice == bungalowDisponibiliDataGridView.SelectedRows[0].Cells[0].Value)
+                {
+                    return b;
+                }
+            }
+            return null;
         }
 
         private void selectRightTab(Button newSelectedButton)
@@ -183,6 +202,10 @@ namespace Gss.View
             {
                 double prezzoBungalow = prenotazioniController.GetSpesaBungalow(b,dataInizioPrenotazioneTimePicker.Value,dataFinePrenotazioneTimePicker.Value,numeroPersone);
                 bungalowDisponibiliDataGridView.Rows.Add(b.Codice, b.GetNumeroStanze(), infoBungalow(b), b.PostiTotaliStandard(), b.PostiTotaliMax(), prezzoBungalow);
+            }
+            if (bungalowDisponibiliDataGridView.Rows.Count == 0)
+            {
+                avantiConfermaButton.Enabled = false;
             }
         }
 

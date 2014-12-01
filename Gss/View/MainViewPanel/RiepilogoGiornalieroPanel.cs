59739d3a74deb60afbb0892a8e7916d2906514b4
pasqualepresutti <paskuale_992@hotmail.it>
2014-12-01 10:58:03 +0100

    Iniziato riepilogoPanel

--- Gss/View/MainViewPanel/RiepilogoGiornalieroPanel.cs
+++ Gss/View/MainViewPanel/RiepilogoGiornalieroPanel.cs
@@ -5,33 +5,57 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Gss.Model;
+using Gss.Controller;
 
 namespace Gss.View.MainViewPanel
 {
     public partial class RiepilogoGiornalieroPanel : System.Windows.Forms.UserControl
     {
+        private PrenotazioniController prenotazioniController;
+
         public RiepilogoGiornalieroPanel()
         {
             InitializeComponent();
         }
 
-        public RiepilogoGiornalieroPanel(IContainer container)
+        public RiepilogoGiornalieroPanel(IContainer container, PrenotazioniController prenotazioniController)
         {
+            this.prenotazioniController = prenotazioniController;
             container.Add(this);
 
             InitializeComponent();
-
-            
-            clientiInArrivoOggiDataGridView.Rows.Add("Vincenzo Villani");
-            clientiInArrivoOggiDataGridView.Rows.Add("Antonio Benincasa");
-            clientiInArrivoOggiDataGridView.Rows.Add("Pasquale Presutti");
-            clientiInArrivoOggiDataGridView.Rows.Add("Unnome Molto Lungo e Art.");
-
-            clientiInPartenzaOggiDataGridView.Rows.Add("Niola Mignogna");
-            clientiInPartenzaOggiDataGridView.Rows.Add("Anthony De Castro Braciola");
-
-            prenotazioniDaSaldareOggiDataGridView.Rows.Add("231 - Nicola Mignogna");
              
         }
+
+        public override void Refresh()
+        {
+            base.Refresh();
+            clientiInArrivoOggiDataGridView.Rows.Clear();
+            clientiInPartenzaOggiDataGridView.Rows.Clear();
+            prenotazioniDaSaldareOggiDataGridView.Rows.Clear();
+            RiempiGrids();
+        }
+        private void RiepilogoGiornalieroPanel_Load(object sender, EventArgs e)
+        {
+            RiempiGrids();
+        }
+
+        private void RiempiGrids()
+        {
+            foreach (Prenotazione p in prenotazioniController.GetPrenotazioniIniziateOggi().ListaPrenotazioni)
+            {
+                clientiInArrivoOggiDataGridView.Rows.Add(p.Cliente.Nome);
+            }
+            foreach (Prenotazione p in prenotazioniController.GetPrenotazioniConcluseOggi().ListaPrenotazioni)
+            {
+                clientiInPartenzaOggiDataGridView.Rows.Add(p.Cliente.Nome);
+            }
+            foreach (Prenotazione p in prenotazioniController.GetPrenotazioniConcluseNonArchiviate().ListaPrenotazioni)
+            {
+                prenotazioniDaSaldareOggiDataGridView.Rows.Add(p.NumeroPrenotazione + " - " + p.Cliente.Nome);
+            }
+        }
+
     }
 }

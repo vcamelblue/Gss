987d9195c430ead065de05a27de5727cd51f4b3f
pasqualepresutti <paskuale_992@hotmail.it>
2014-12-01 17:18:48 +0100

    Completato GestioneClientiPanel

--- Gss/View/MainViewPanel/GestioneClientiPanel.cs
+++ Gss/View/MainViewPanel/GestioneClientiPanel.cs
@@ -5,13 +5,26 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using Gss.Controller;
+using Gss.Model;
 
 namespace Gss.View.MainViewPanel
 {
     public partial class GestioneClientiPanel : System.Windows.Forms.UserControl
     {
+        private ClientiController clientiController;
+
         public GestioneClientiPanel()
         {
+            InitializeComponent();
+        }
+
+        public GestioneClientiPanel(IContainer container, ClientiController clientiController)
+        {
+            this.clientiController = clientiController;
+            container.Add(this);
+
             InitializeComponent();
         }
 
@@ -21,5 +34,80 @@ namespace Gss.View.MainViewPanel
 
             InitializeComponent();
         }
+
+        private void GestioneClientiPanel_Load(object sender, EventArgs e)
+        {
+            RiempiGrigliaClienti();
+        }
+
+        private void RiempiGrigliaClienti()
+        {
+            foreach (Cliente c in clientiController.GetAllClienti().ListaClienti)
+            {
+                clientiDataGridView.Rows.Add(c.Nome, c.Cognome, c.CodiceFiscale, c.Telefono, c.Email);
+            }
+        }
+
+        public override void Refresh()
+        {
+            base.Refresh();
+            clientiDataGridView.Rows.Clear();
+            RiempiGrigliaClienti();
+        }
+
+        private void visualizzaClienteButton_Click(object sender, EventArgs e)
+        {
+            string codicefiscale = clientiDataGridView.SelectedRows[0].Cells[2].Value.ToString();
+            Cliente clienteSelezionato = clientiController.GetClienteByCF(codicefiscale);
+
+            AggiungiModificaCliente visualizzaClienteForm = new AggiungiModificaCliente(clientiController, clienteSelezionato, true);
+            visualizzaClienteForm.Show();
+        }
+
+        private void rimuoviClienteButton_Click(object sender, EventArgs e)
+        {
+            string codicefiscale = clientiDataGridView.SelectedRows[0].Cells[2].Value.ToString();
+            Cliente clienteSelezionato = clientiController.GetClienteByCF(codicefiscale);
+
+            DialogResult res = MessageBox.Show("Sicuro di voler rimuovere il cliente selezionato?", "Rimozione Cliente", MessageBoxButtons.OKCancel);
+            if (res == DialogResult.OK)
+            {
+
+            }
+            else
+            {
+
+            }
+            
+        }
+
+        private void aggiungiClienteButton_Click(object sender, EventArgs e)
+        {
+            AggiungiModificaCliente aggiungiClienteForm = new AggiungiModificaCliente(clientiController);
+
+            DialogResult res = aggiungiClienteForm.ShowDialog();
+            if (res == DialogResult.OK)
+            {
+                Refresh();
+            }
+        }
+
+        private void modificaClienteButton_Click(object sender, EventArgs e)
+        {
+            string codicefiscale = clientiDataGridView.SelectedRows[0].Cells[2].Value.ToString();
+            Cliente clienteSelezionato = clientiController.GetClienteByCF(codicefiscale);
+
+            AggiungiModificaCliente modificaClienteForm = new AggiungiModificaCliente(clientiController, clienteSelezionato, false);
+
+            DialogResult res = modificaClienteForm.ShowDialog();
+            if (res == DialogResult.OK)
+            {
+                Refresh();
+            }
+        }
+
+
+
+        
     }
 }

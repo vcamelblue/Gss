6828835f5fe74095e69d38c2473e8adadbe24f13
vcamelblue <vincvill@hotmail.it>
2014-12-17 17:51:04 +0100

    Completate modifiche GestioneImpianti

--- Gss/View/GestioneImpianti.cs
+++ Gss/View/GestioneImpianti.cs
@@ -30,35 +30,36 @@ namespace Gss.View
 
         private void visualizzaImpiantoButton_Click(object sender, EventArgs e)
         {
-            foreach (Impianto i in resortController.GetImpianti().ListaImpianti)
+            try
             {
-                if (i.Codice==impiantiDataGridView.SelectedRows[0].Cells[0].Value)
-                {
-                    VisualizzaImpianto visualizzaImpiantoForm = new VisualizzaImpianto(i);
-                    visualizzaImpiantoForm.Show();
-                }
+                Impianto impiantoToShow = resortController.GetImpiantoByCodice(impiantiDataGridView.SelectedRows[0].Cells[0].Value.ToString());
+
+                VisualizzaImpianto visualizzaImpiantoForm = new VisualizzaImpianto(impiantoToShow);
+                visualizzaImpiantoForm.Show();            
             }
-            
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
         }
 
         private void rimuoviImpiantoButton_Click(object sender, EventArgs e)
         {
-            foreach (Impianto i in resortController.GetImpianti().ListaImpianti)
+            try
             {
-                if (i.Codice == impiantiDataGridView.SelectedRows[0].Cells[0].Value)
+                Impianto impiantoToRemove = resortController.GetImpiantoByCodice(impiantiDataGridView.SelectedRows[0].Cells[0].Value.ToString());
+            
+                DialogResult result = MessageBox.Show("Sicuro di voler rimuovere l'impianto selezionato?", "Rimozione Pista", MessageBoxButtons.OKCancel);
+            
+                if (result == DialogResult.OK)
                 {
-                    DialogResult result = MessageBox.Show("Sicuro di voler rimuovere l'impianto selezionato?", "Rimozione Pista", MessageBoxButtons.OKCancel);
-                    if (result == DialogResult.OK)
-                    {
-                        resortController.RemoveImpianto(i);
-                        Refresh();
-                        break;
-                    }
-                    else if (result == DialogResult.Cancel)
-                    {
-                        break;
-                    }
+                    resortController.RemoveImpianto(impiantoToRemove);
+                    Refresh();
                 }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
             }
         }
 
@@ -75,33 +76,36 @@ namespace Gss.View
 
         private void modificaImpiantoButton_Click(object sender, EventArgs e)
         {
-            foreach (Impianto i in resortController.GetImpianti().ListaImpianti)
+            try
             {
-                if (i.Codice == impiantiDataGridView.SelectedRows[0].Cells[0].Value)
-                {
-                    Impianto impiantoCopia = (Impianto)i.Clone();
-                    AggiungiModificaImpianto modificaImpianto = new AggiungiModificaImpianto(resortController, impiantoCopia);
+                Impianto impiantoToEdit = resortController.GetImpiantoByCodice(impiantiDataGridView.SelectedRows[0].Cells[0].Value.ToString());
+            
+                Impianto impiantoCopia = (Impianto)impiantoToEdit.Clone();
+            
+                AggiungiModificaImpianto modificaImpianto = new AggiungiModificaImpianto(resortController, impiantoCopia);
 
-                    DialogResult res = modificaImpianto.ShowDialog();
-                    if (res == DialogResult.OK)
-                    {
-                        resortController.EditImpianto(i, impiantoCopia);
-                        Refresh();
-                        break;
-                    }
+                DialogResult res = modificaImpianto.ShowDialog();
+                if (res == DialogResult.OK)
+                {
+                    resortController.EditImpianto(impiantoToEdit, impiantoCopia);
+                    Refresh();
                 }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
             }
         }
         
         public override void Refresh()
         {
             base.Refresh();
-            impiantiDataGridView.Rows.Clear();
             RiempiDataGrid();
         }
         
         private void RiempiDataGrid()
         {
+            impiantiDataGridView.Rows.Clear();
             foreach (Impianto i in resortController.GetImpianti().ListaImpianti)
             {
                 impiantiDataGridView.Rows.Add(i.Codice, i.Nome, i.Versante, i.GetPisteAlpine().Count, i.GetPisteFondo().Count, i.GetPisteSnowPark().Count);
